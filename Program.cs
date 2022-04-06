6b81223c89e2b20ea00765111c81aa78820f6fdd
Alexander <alexander19.evsyukov97@gmail.com>
2022-04-06 21:40:24 +0300

    add magic program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо равна 3 символа.
+Первоначальный массив можно ввести с клавиатуры, либо задать на
+старте выполнения алгоритма. При решении не рекомендуется
+пользоваться коллекциями, лучше обойтись исключительно массивами. */
+
+string[] array1 = { "program", "git", "id", "internet", "2" };
+string[] array2 = new string[array1.Length];
+
+void Condition(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+        
+    }
+}
+Condition(array1, array2);
+Print(array2);
